c9aacaf02f6fe3927cbe58ab177135678728e200
leorossi96 <rl.rossileonardo@gmail.com>
2018-10-24 15:47:03 +0200

    Counter Aggiunto

---
+++ Progetto_AUI_unity/Assets/Scripts/CollectiblesCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesCounter : MonoBehaviour {
+
+    public int counter;
+    public int nCollectibles;
+
+    public void Start()
+    {
+        nCollectibles = GameObject.FindGameObjectsWithTag("Collectible").Length;
+        Debug.Log(nCollectibles);
+    }
+
+    public void CollectibleFound(){
+        counter++;
+        Debug.Log(counter);
+        if(counter == nCollectibles){
+            Debug.Log("HAI VINTO");
+        }
+    }
+}
---
+++ Progetto_AUI_unity/Assets/Scripts/PlayerCollisionSearch.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using UnityStandardAssets.Utility;
+using System.Net;
+using System.Security.Cryptography;
+
+public class PlayerCollisionSearch : MonoBehaviour
+{
+
+    public PlayerMovementSearch movement;
+
+    public CollectiblesCounter counter;
+
+    public Transform terrain;
+
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Collectible")
+        {
+
+            Debug.Log("Collectible found");
+
+            Vector3 newCollectiblePosition = new Vector3(collider.transform.position.x, terrain.position.y, collider.transform.position.z);
+            collider.transform.SetPositionAndRotation(newCollectiblePosition, collider.transform.rotation);
+
+            counter.CollectibleFound();
+        }
+    }
+
+
+
+    void Update()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
