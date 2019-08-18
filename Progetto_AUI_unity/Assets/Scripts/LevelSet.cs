835bf43a8792399c211a163a2fcd7d5f2137b194
marcomine <marco2.minelli@mail.polimi.it>
2019-08-18 19:26:26 +0100

    Added right modified parameter on unity for level search and adjust the menu

--- Progetto_AUI_unity/Assets/Scripts/LevelSet.cs
+++ Progetto_AUI_unity/Assets/Scripts/LevelSet.cs
@@ -7,18 +7,20 @@ using System.Text;
 public class LevelSet : MonoBehaviour {
 
     public LevelRun[] levelRun;
-    public LevelSearch levelSearch;
+    public LevelSearch[] levelSearch;
     public ZoneLevelSearch[] zoneLevelSearch;
-
+    
 
     public void StartCoroutine(Login login)
     {
+        Debug.Log("LOGIN.SELECTED PATIENT: " + login.selectedPatient);
         string json = JsonUtility.ToJson(login.selectedPatient);
         Debug.Log("String json awake" + json);
-        StartCoroutine(SendLevelPost(json));
+        StartCoroutine(SendLevelPostRun(json));
+        StartCoroutine(SendLevelPostSearch(json));
     }
 
-    IEnumerator SendLevelPost(string json)
+    IEnumerator SendLevelPostRun(string json)
     {
         Debug.Log("entro nella coroutine");
         var request = new UnityWebRequest("http://127.0.0.1:5000/prova/levels/run", "POST");
@@ -40,11 +42,57 @@ public class LevelSet : MonoBehaviour {
         string jsonString = request.downloadHandler.text;
         levelRun = JsonHelper.getJsonArray<LevelRun>(jsonString);
         Debug.Log(levelRun[0].name);
+    }
+
+    IEnumerator SendLevelPostSearch(string json)
+    {
+        Debug.Log("entro nella coroutine");
+        var request = new UnityWebRequest("http://127.0.0.1:5000/prova/levels/search", "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        Debug.Log(request.downloadHandler.text);
 
 
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log("questo e' l'errore" + request.error);
+        }
 
 
+        string jsonString = request.downloadHandler.text;
+        levelSearch = JsonHelper.getJsonArray<LevelSearch>(jsonString);
+        Debug.Log(levelSearch[0].id);
+        string json_z = JsonUtility.ToJson(levelSearch[0]);
+        Debug.Log("String json ZONES COROUTINE" + json_z);
+        StartCoroutine(SendLevelPostZones(json_z));
+    }
 
+    IEnumerator SendLevelPostZones(string json_z)
+    {
+        Debug.Log("entro nella coroutine");
+        var request = new UnityWebRequest("http://127.0.0.1:5000/prova/levels/search/zones", "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json_z);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        Debug.Log(request.downloadHandler.text);
+
+
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log("questo e' l'errore" + request.error);
+        }
+
+
+        string jsonString = request.downloadHandler.text;
+        zoneLevelSearch = JsonHelper.getJsonArray<ZoneLevelSearch>(jsonString);
+        Debug.Log(zoneLevelSearch[0].number);
     }
 
 
--- Progetto_AUI_unity/Assets/Scripts/PatientShowSearch.cs
+++ Progetto_AUI_unity/Assets/Scripts/PatientShowSearch.cs
@@ -1,56 +1,81 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class PatientShowSearch : MonoBehaviour {
 
     public Login login;
     public LevelSet levelSet;
     public GameObject patientShow;
+    InputField[] inpfields;
+    public Button addButton;
+    public int numberOfZones;
 
 
-
-
-
-    /*
-
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
+        addButton.onClick.AddListener(TaskOnClick);
         TextMeshProUGUI textPatient = patientShow.GetComponentInChildren<TextMeshProUGUI>();
         textPatient.text = login.selectedPatient.last_name + " " + login.selectedPatient.first_name;
         // textPatient.GetComponent<GUIText>().text = login.selectedPatient.last_name + " " + login.selectedPatient.first_name;
-        InputField[] inpfields = patientShow.GetComponentsInChildren<InputField>();
-        Debug.Log("UPDATE PATIENT SHOW SEARCH POWERUP: " + levelSet.levelSearch.power_up);
-        for(int i=0; i < inpfields.Length; i++)
+        inpfields = patientShow.GetComponentsInChildren<InputField>();
+        numberOfZones = levelSet.zoneLevelSearch.Length;
+        Debug.Log("THE ARRAY OF ZONE HAS A LENGHT OF: " + numberOfZones);
+        for(int i=numberOfZones; i < inpfields.Length; i++)
         {
-            if(inpfields[i].name == "StaticObstacle")
+            string str_i = i.ToString();
+            if (inpfields[i].name == ("Zone_Search_" + str_i));
             {
-                inpfields[i].text = levelSet.levelSearch.static_obstacle.ToString();
+                inpfields[i].gameObject.SetActive(false);
+            }
+        }
+
+        for(int i=0; i < numberOfZones; i++)
+        {
+            if(inpfields[i].name == "Zone_Search_" + i.ToString())
+            {
+                inpfields[i].text = levelSet.zoneLevelSearch[i].number_stars_per_zone.ToString();
             }
 
-            if (inpfields[i].name == "DynamicObstacle")
-            {
-                inpfields[i].text = levelSet.levelSearch.dynamic_obstacle.ToString();
-            }
-
-            if (inpfields[i].name == "MaxTime")
-            {
-                inpfields[i].text = levelSet.levelSearch.max_time.ToString();
-            }
-
-            if (inpfields[i].name == "PowerUp")
-            {
-                inpfields[i].text = levelSet.levelSearch.power_up.ToString();
-            }
-
-            if (inpfields[i].name == "Lives")
-            {
-                inpfields[i].text = levelSet.levelSearch.lives.ToString();
-            }
         }
 
 
 
-    }*/
+    }
+
+    void Update()
+    {
+        numberOfZones = levelSet.zoneLevelSearch.Length;
+        Debug.Log("THE ARRAY OF ZONE HAS A LENGHT OF: " + numberOfZones);
+        for (int i = numberOfZones; i < inpfields.Length; i++)
+        {
+            string str_i = i.ToString();
+            if (inpfields[i].name == ("Zone_Search_" + str_i)) ;
+            {
+                inpfields[i].gameObject.SetActive(false);
+            }
+        }
+
+        for (int i = 0; i < numberOfZones; i++)
+        {
+            if (inpfields[i].name == "Zone_Search_" + i.ToString())
+            {
+                inpfields[i].text = levelSet.zoneLevelSearch[i].number_stars_per_zone.ToString();
+            }
+
+        }
+
+    }
+
+
+    public void TaskOnClick()
+    {
+        Debug.Log("SONO DENTRO ADD BUTTON");
+        int new_size = numberOfZones + 1;
+        Array.Resize(ref levelSet.zoneLevelSearch, new_size);
+        levelSet.zoneLevelSearch[new_size - 1].number = new_size;
+        levelSet.zoneLevelSearch[new_size - 1].number_stars_per_zone = 3;
+    }
+
 }
