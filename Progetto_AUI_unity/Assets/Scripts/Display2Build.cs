b9e32d9ac745d47cd5a0c74d12604743532492fa
leorossi96 <rl.rossileonardo@gmail.com>
2019-09-19 17:19:00 +0200

    Display build Fix

---
+++ Progetto_AUI_unity/Assets/Scripts/Display2Build.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display2Build : MonoBehaviour {
+
+    private void Awake()
+    {
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
